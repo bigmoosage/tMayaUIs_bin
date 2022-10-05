4683a45eb4dc51905a8637c615ea040852866bf2
bigmoosage <42448298+bigmoosage@users.noreply.github.com>
2022-10-05 20:38:30 +1100

    Update userSetup.py

--- userSetup.py
+++ userSetup.py
@@ -1,12 +1,15 @@
+# redundant python sys import
 import sys
 
-
+# redundant for future execution deferment if needed
 from maya.utils import executeDeferred
 
+# basic command imports for command stability on maya open
 import pymel.core as pm
 import maya.cmds as cd
 import maya.mel as melE
 
+# specific important reqs from plug-in again for redundancy if import failure at other point
 from tMayaUIs_bin import gui
 from tMayaUIs_bin.gui import gui_Window
 from tMayaUIs_bin.operations import ops_joints as oj
